75535bb0625388d6302987a6d87c5244f868f352
hook <hook@vap0r>
2018-04-25 02:54:14 +0100

    Addition of helloworld script, gitignore

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace curly_bois_shell_script
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
